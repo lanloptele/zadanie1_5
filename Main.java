1555397a8ebc3c6264602861cc2c21e895280414
lanloptele <100698914+lanloptele@users.noreply.github.com>
2022-03-23 17:51:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int licznik=49;
+    while(licznik<100){
+      licznik++;
+      System.out.println(+ licznik);
+    }
+    System.out.println("koniec");
+  }
+}
