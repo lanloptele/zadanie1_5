4d6407dd713c12a80877561ae80dc21ed33c2a8e
lanloptele <100698914+lanloptele@users.noreply.github.com>
2022-03-23 18:13:58 +0000

    działa

--- Main.java
+++ Main.java
@@ -3,7 +3,12 @@ class Main {
     int licznik=49;
     while(licznik<100){
       licznik++;
-      System.out.println(+ licznik);
+      
+      if(licznik % 2==0 ){//czyli liczba dzielona przez dwa jeżeli ma resztę tzn że nieparzysta
+       System.out.println(+ licznik);
+      } else{
+        
+      }
     }
     System.out.println("koniec");
   }
